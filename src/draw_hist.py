97b5a0ae3ed3d52a9cf647c63498aaf1d76d90ed
stormslowly <stormslowly@gmail.com>
2013-05-24 00:15:13 +0800

    extrac function

--- src/draw_hist.py
+++ src/draw_hist.py
@@ -6,6 +6,25 @@ Created on May 21, 2013
 #import matplotlib.pyplot as plot
 # from pylab import hist,plot
 
+
+def gen_json(line_rep,middle_rep):
+
+    head_str = """{"name":"CodeFacec", "children":[\n"""
+ 
+
+    outfile = open('category_ratio.json','w')
+    outfile.write(head_str)
+    
+    for item in line_rep:
+        if item[0] <middle_rep:
+            outfile.write("""\t,{"name":"O","size":%f}\n""" %(item[1]*1.0/total_line_num) )
+        else:
+            outfile.write("""\t,{"name":"N","size":%f}\n""" %(item[1]*1.0/total_line_num) )
+    
+    outfile.write(""" ]}""")
+    
+    outfile.close();
+
 if __name__ == '__main__':
     f = open('hist.txt','r');
     reps = [];
@@ -32,21 +51,7 @@ if __name__ == '__main__':
     
 #     print line_rep
 
-    head_str = """{"name":"CodeFacec", "children":[\n"""
- 
-
-    outfile = open('category_ratio.json','w')
-    outfile.write(head_str)
-    
-    for item in line_rep:
-        if item[0] <middle_rep:
-            outfile.write("""\t,{"name":"O","size":%f}\n""" %(item[1]*1.0/total_line_num) )
-        else:
-            outfile.write("""\t,{"name":"N","size":%f}\n""" %(item[1]*1.0/total_line_num) )
-    
-    outfile.write(""" ]}""")
-    
-    outfile.close();
+    gen_json(line_rep,middle_rep)
     print lines[0]*1.0/total_line_num
     
 #     plot.bar(reps,lines)
